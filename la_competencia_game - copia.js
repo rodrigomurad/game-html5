d6a67f4744c0ed4bdcb178f802d78e7478a4e4d6
rodrigo murad <rodrigo.murad@gmail.com>
2024-06-23 21:11:26 -0300

    TP4 (TIFMA)

--- la_competencia_game - copia.js
+++ la_competencia_game - copia.js
@@ -66,7 +66,7 @@ function rankingGenerate(data) {
 };
 // Fin WebSocket ###################################################################################################
 
-// Giroscopio ######################################################################################################
+// TP4 #############################################################################################################
 window.addEventListener('deviceorientation', handleOrientation);
 
 function handleOrientation(event) {
@@ -89,24 +89,20 @@ function handleOrientation(event) {
             nombre_luchador_usuario.innerText = "Firex";
             energiaFighterUsuario = 1;
             seleccionado = true;
+        } else if (gamma > 40 && seleccionado == false) {
+            console.log("Earthak");
+            fighter_seleccionado.src = document.getElementById("Earthak").src;
+            nombre_luchador_usuario.innerText = "Earthak";
+            energiaFighterUsuario = 0;
+            seleccionado = true;
         } else {
-            if (gamma > 40 && seleccionado == false) {
-                console.log("Earthak");
-                fighter_seleccionado.src = document.getElementById("Earthak").src;
-                nombre_luchador_usuario.innerText = "Earthak";
-                energiaFighterUsuario = 0;
-                seleccionado = true;
-            }
+            fighter_seleccionado.src = "";
+            nombre_luchador_usuario.innerText = "";
+
+            document.getElementById("nombre_luchador_PC").innerText = "";
+            document.getElementById("fighter_PC").src = "";
+            seleccionado = false;
         }
-    }
-
-    if (beta >= 30 && gamma >= -40 && gamma <= 40) {
-        fighter_seleccionado.src = "";
-        nombre_luchador_usuario.innerText = "";
-
-        document.getElementById("nombre_luchador_PC").innerText = "";
-        document.getElementById("fighter_PC").src = "";
-        seleccionado = false;
     }
 
     if (seleccionado == true) {
@@ -144,7 +140,6 @@ function handleOrientation(event) {
 
         document.getElementById("puntaje_usuario").innerText = puntaje_usuario;
         document.getElementById("puntaje_PC").innerText = puntaje_PC;
-        seleccionado = false;
 
         // Guardamos el Resultado en el localStorage
         ls.setItem("Resultado", texto_resultado.innerText);
@@ -160,9 +155,126 @@ function handleOrientation(event) {
         ws.send(JSON.stringify(data));
         // Fin WebSocket ###################################################################################################
     }
+
+
 }
 
-// Fin Giroscopio ##################################################################################################
+
+// #################################################################################################################
+// Comienza el Evento de Drag & Drop
+// luchadores.addEventListener('dragstart', e => {
+//     // Guardamos Año, Mes, Día, Hora, Minutos, Segundos y Milisegundos en formato ISO en que Comienza el Drag & Drop
+//     let recuperador_fecha = new Date();
+//     anio = recuperador_fecha.getFullYear();
+//     mes = (recuperador_fecha.getMonth() + 1).toString().padStart(2, '0');
+//     dia = recuperador_fecha.getDate().toString().padStart(2, '0');
+//     tiempo = (recuperador_fecha.getHours()).toString().padStart(2, '0');
+//     tiempo += (recuperador_fecha.getMinutes()).toString().padStart(2, '0');
+//     tiempo += (recuperador_fecha.getSeconds()).toString().padStart(2, '0');
+//     tiempo += (recuperador_fecha.getMilliseconds()).toString().padStart(3, '0');
+
+//     seleccionado = false;
+//     fighter_seleccionado.src = "";
+//     drop_zone.classList.add("dropZoneStyleActive");
+//     drop_zone.style.visibility = "visible";
+
+//     e.dataTransfer.setData('nombreFighterUsuario', e.target.id);
+//     energiaFighterUsuario = document.getElementById(e.target.id).dataset.energy;
+//     e.dataTransfer.setData('imagenFighterUsuario', e.target.src);
+
+//     document.getElementById("nombre_luchador_PC").innerText = "";
+//     document.getElementById("fighter_PC").src = "";
+// });
+
+
+
+
+// Si el Luchador es soltado fuera del Círculo Rojo, entonces se oculta nuevamente el Círculo Rojo
+// luchadores.addEventListener('dragend', e => {
+//     if (seleccionado == false) {
+//         drop_zone.style.visibility = "hidden";
+//     }
+// });
+
+
+
+
+// Previene el comportamiento normal, para que se permita el evento Drag & Drop
+// drop_zone.addEventListener("dragover", e => {
+//     e.preventDefault();
+// });
+
+
+
+// El Luchador es soltado dentro del Círculo Rojo
+// drop_zone1.addEventListener("drop", e => {
+// localStorage
+// Guardamos Año, Mes, Día, Hora, Minutos, Segundos y Milisegundos en formato ISO en que Finaliza el Drag & Drop
+// ls.setItem("StartDragAndDrop", anio + mes + dia + "T" + tiempo);
+
+// let recuperador_fecha = new Date();
+// anio = recuperador_fecha.getFullYear();
+// mes = (recuperador_fecha.getMonth() + 1).toString().padStart(2, '0');
+// dia = recuperador_fecha.getDate().toString().padStart(2, '0');
+// tiempo = (recuperador_fecha.getHours()).toString().padStart(2, '0');
+// tiempo += (recuperador_fecha.getMinutes()).toString().padStart(2, '0');
+// tiempo += (recuperador_fecha.getSeconds()).toString().padStart(2, '0');
+// tiempo += (recuperador_fecha.getMilliseconds()).toString().padStart(3, '0');
+// ls.setItem("EndDragAndDrop", anio + mes + dia + "T" + tiempo);
+
+// seleccionado = true;
+// drop_zone.classList.remove("dropZoneStyleActive");
+// nombre_luchador_usuario.innerText = e.dataTransfer.getData('nombreFighterUsuario');
+// fighter_seleccionado.src = e.dataTransfer.getData('imagenFighterUsuario');
+
+// /* Eliger un Número entre 0 y 29, luego lo divide entre 10, y al ser un "parseInt", solo reserva la
+// parte entera, quedando un número entre 0 y 2. Con ese número, busca el valor en el array "nombresluchadores",
+// y lo utiliza para generar toda la info del Luchador de la PC */
+// energiaFighterPC = parseInt((Math.random() * 30) / 10);
+// nombreFighterPC = nombresluchadores[energiaFighterPC];
+// document.getElementById("nombre_luchador_PC").innerText = nombreFighterPC;
+// document.getElementById("fighter_PC").src = "img/fighter_" + nombreFighterPC + ".png";
+
+// /* Chequeamos quién ganó, esto lo hacemos restando los valores:
+// Le restamos a la Energía del Usuario la Energía de la PC,
+// si el valor queda negativo, entonces el Usuario pierde, de lo contrario gana */
+// if (energiaFighterUsuario == energiaFighterPC) {
+//     texto_resultado.style.color = "#333";
+//     texto_resultado.innerText = "empate";
+// } else {
+//     if (energiaFighterUsuario == 2 && energiaFighterPC == 0 || energiaFighterUsuario == 0 && energiaFighterPC == 2) {
+//         energiaPivote = energiaFighterUsuario;
+//         energiaFighterUsuario = energiaFighterPC;
+//         energiaFighterPC = energiaPivote;
+//     }
+//     if (energiaFighterUsuario - energiaFighterPC < 0) {
+//         texto_resultado.style.color = "#f00";
+//         texto_resultado.innerText = "Perdiste";
+//         puntaje_PC++;
+
+//     } else {
+//         texto_resultado.style.color = "#00f";
+//         texto_resultado.innerText = "Ganaste";
+//         puntaje_usuario++;
+//     }
+// }
+
+// document.getElementById("puntaje_usuario").innerText = puntaje_usuario;
+// document.getElementById("puntaje_PC").innerText = puntaje_PC;
+
+// ls.setItem("Resultado", texto_resultado.innerText);
+
+// // WebSocket #######################################################################################################
+// var data = {
+//     game: 'La Competencia',
+//     event: 'posicion',
+//     player: window.player,
+//     value: puntaje_usuario - 1
+// };
+// data.value = data.value + 1;
+// ws.send(JSON.stringify(data));
+// // Fin WebSocket ###################################################################################################
+// });
 
 // Reiniciamos todos los Valores
 function replay() {
